8d76e1860fceb1f329838196c1c2c913d9a1c9e5
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2024-11-11 15:30:18 +0530

    Send PONG response on ping

--- app/server.go
+++ app/server.go
@@ -16,9 +16,15 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	//fmt.Println("Listening on port 6379")
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+	_, err = conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		return
 	}
 }
